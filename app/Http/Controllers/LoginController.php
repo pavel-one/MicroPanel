3596a8a2cd1ae77c9dd3ab3ee14b75003ed40fe9
Pavel Zarubin <pavel@orendat.ru>
2020-01-26 03:58:25 +0300

    #2 Finish

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -100,7 +100,7 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return route('index');
+        return redirect()->route('index');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,15 @@ Route::get('/', 'PageController@index')->middleware('guest')->name('index');
 Route::get('/home', 'HomeController@index'); //TODO: DEL
 
 
-//Авторизация/Региастрация
+//Авторизация/Регистрация
 Route::group(['prefix' => 'login', 'middleware' => 'guest'], static function () {
     Route::get('/', 'LoginController@index')->name('sign in');
     Route::post('/', 'LoginController@authenticate')->name('login');
 
     Route::get('register', 'LoginController@registerView');
     Route::post('register', 'LoginController@register')->name('register');
-
-    Route::post('logout', 'LoginController@logout')->name('logout');
 });
+Route::post('logout', 'LoginController@logout')->name('logout');
 
 //Панель управления
 Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], static function () {
